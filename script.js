2382d213c6afc56dcd6810a09edf728f8de1f1c4
Stephen Reynolds <sdreynolds110@gmail.com>
2019-11-23 17:50:08 -0500

    editing js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function generate(){
-    var complexity = document.getElementById("slider").value;
+    var sliderValues = document.getElementById("slider").values;
     var values = ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+");
     var password = "";
 
-    for(var i = 0; i <= complexity; i++){
+    for(var i = 0; i <= sliderValues; i++){
         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
     }
-    document.getElementById("display").values = password;
+    document.getElementById("display").value = password;
+
     document.getElementById("lastPasswords").innerHTML += password + "<br>";
+    console.log(password)
+}
+function clipboard(){
+    document.getElementById("display").select();
+    document.execCommand("Copy");
+    alert("Your password is copied!")
+
 }
