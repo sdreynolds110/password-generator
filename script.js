d7b08b251f325d91a3c0f9b879a2dcd359a66da7
Stephen Reynolds <sdreynolds110@gmail.com>
2019-11-23 16:52:59 -0500

    added css and js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function generatePassword(){
-    let complexity = document.getElementById("slider").value;
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
-    let password = "";
+function generate(){
+    var complexity = document.getElementById("slider").value;
+    var values = ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+");
+    var password = "";
 
-    for(var i = 0; i <== complexity; i++){
+    for(var i = 0; i <= complexity; i++){
         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
     }
-
-    document.getElementById("display").value = password;
+    document.getElementById("display").values = password;
+    document.getElementById("lastPasswords").innerHTML += password + "<br>";
 }
