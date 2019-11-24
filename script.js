78a8cfc68c7050f4a792b9ac5191d25c19ec2ef1
Stephen Reynolds <sdreynolds110@gmail.com>
2019-11-23 21:30:43 -0500

    made successful js changes

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function generate(){
-    var sliderValues = document.getElementById("slider").values;
-    var values = ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+");
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowercase = "abcdefghijklmnopqrstuvwxyz";
+    var nums = "1234567890";
+    var syms = "!@#$%^&*()_+";
+    var all = uppercase + lowercase + nums + syms;
+    var sliderValues = document.getElementById("slider").value;
     var password = "";
 
     for(var i = 0; i <= sliderValues; i++){
-        password += values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+        password = password + all.charAt(Math.floor(Math.random() * Math.floor(all.length - 1)));
     }
-    document.getElementById("display").values = password;
+    document.getElementById("display").value = password;
 
     document.getElementById("lastPasswords").innerHTML += password + "<br>";
     console.log(password)
@@ -15,5 +19,4 @@ function clipboard(){
     document.getElementById("display").select();
     document.execCommand("Copy");
     alert("Your password is copied!")
-
 }
